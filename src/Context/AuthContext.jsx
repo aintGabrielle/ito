91f4b3400d6cfbce5fe2c6ecf9aa1bb2313b7f84
aintGabrielle <isaacdomino.igd@gmail.com>
2025-03-16 18:52:44 +0800

    Redirect pls

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -71,6 +71,9 @@ export const AuthContextProvider = ({ children }) => {
     try {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
+        options: {
+          redirectTo: `${window.location.origin}/assessment`,
+        },
       });
 
       if (error) {
