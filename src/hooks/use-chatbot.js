6f6fee5eb6c485b391b5703ca23d2054afb78a10
Gerald Chavez <chavezgerald23@gmail.com>
2025-04-03 16:52:25 +0800

    fixed missing symbol

--- src/hooks/use-chatbot.js
+++ src/hooks/use-chatbot.js
@@ -22,11 +22,11 @@ const useChatbot = () => {
 		isLoading,
 		error,
 		mutate,
-		//   } = useSWR(`/chat_messages_${user.id}`, async () => fetcher(user.id));
-		//   const [tempMessages, setTempMessages] = useState(messages);
 	} = useSWR(user?.id ? `/chat_messages_${user.id}` : null, async () =>
 		fetcher(user.id),
 	);
+	const [tempMessages, setTempMessages] = useState(messages);
+
 	const sendMessage = async (userInput) => {
 		if (!userInput.trim() || !user) return;
 		const newUserMessage = { role: "user", message: userInput };
